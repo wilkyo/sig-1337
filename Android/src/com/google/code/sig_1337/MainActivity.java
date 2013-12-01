18068a0f4d8ce9f059169d264913f978dc9aed0d
nauja <jeremymorosi@hotmail.fr>
2013-12-01 18:36:18 +0100

    Musique pour faire patienter l'utilisateur au clic sur "Local". Il paraît que si on clic 100 fois sur le bouton quelque chose se produit.

--- Android/src/com/google/code/sig_1337/MainActivity.java
+++ Android/src/com/google/code/sig_1337/MainActivity.java
@@ -2,6 +2,9 @@ package com.google.code.sig_1337;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.media.SoundPool;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
@@ -27,9 +30,14 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		final MediaPlayer mp = MediaPlayer.create(MainActivity.this,
+				R.raw.bouton);
 		((Button) findViewById(R.id.local))
 				.setOnClickListener(new OnClickListener() {
 					public void onClick(View v) {
+						mp.setVolume(1.0f, 1.0f);
+						mp.setAudioStreamType(AudioManager.STREAM_MUSIC);
+						mp.start();
 						startActivity(new Intent(MainActivity.this,
 								LocalActivity.class));
 					}
